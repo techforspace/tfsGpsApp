8b1058bbde7c4a6b2f1c4ed2abfee1311cec7645
root <techforspace@gmail.com>
2015-09-07 19:10:55 +0200

    Threads added! Not displaying table rows

--- app/src/main/java/com/example/amerioch/tfsGpsApp/MainScreen.java
+++ app/src/main/java/com/example/amerioch/tfsGpsApp/MainScreen.java
@@ -25,6 +25,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
@@ -34,39 +35,62 @@ public class MainScreen extends Activity{
     private TableLayout table;
     private Button addFriendsButton;
     private Button buttonLogOut;
+    private GpsClass gps;
+
+    TableLayout rl;
     //Executed when is created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
-        TableLayout rl = (TableLayout) findViewById(R.id.friendTable);
-        //modify this to read each friend and calculate GPS distance
-        ArrayList<String> friends = Connect.dB.getFriends(Connect.username);
-        GpsClass gps = new GpsClass(getApplicationContext());
-        Connect.dB.updatePosition(Connect.username,gps.getLatitude(),gps.getLongitude(),gps.getAltitude());
-        if(friends!=null) {
-            for (String friend : friends) {
-                Location friendLocation = new Location("");
-                //Get position first element latitude, second longitude, third altitude.
-                double[] position = Connect.dB.readPosition(friend);
-                friendLocation.setLatitude(position[0]);
-                friendLocation.setLongitude(position[1]);
-                Double distance = gps.calculateDistance(gps.getLocation(), friendLocation);
-                String distanceStr;
-                if (distance >= 1000.0) {
-                    distance = distance / 1000.0;
-                    distanceStr = distance.toString() + "km";
-                } else {
-                    distanceStr = distance.toString() + "m";
+        gps = new GpsClass(getApplicationContext());
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    DataBaseInteraction dBInteraction = new DataBaseInteraction(AccountData.URLDB, AccountData.PASS, AccountData.USERNAME);
+                    dBInteraction.connectToDB();
+                    rl = (TableLayout) findViewById(R.id.friendTable);
+                    //modify this to read each friend and calculate GPS distance
+                    ArrayList<String> friends = dBInteraction.getFriends(Connect.username);
+                    dBInteraction.updatePosition(Connect.username,gps.getLatitude(),gps.getLongitude(),gps.getAltitude());
+                    if(friends.size()>0) {
+                        for (String friend : friends) {
+                            Location friendLocation = new Location("");
+                            //Get position first element latitude, second longitude, third altitude.
+                            double[] position = dBInteraction.readPosition(friend);
+                            friendLocation.setLatitude(position[0]);
+                            friendLocation.setLongitude(position[1]);
+                            Double distance = gps.calculateDistance(gps.getLocation(), friendLocation);
+                            String distanceStr;
+                            if (distance >= 1000.0) {
+                                distance = distance / 1000.0;
+                                distanceStr = distance.toString() + "km";
+                            } else {
+                                distanceStr = distance.toString() + "m";
+                            }
+                            writeRow(friend, distanceStr);
+                        }
+                    }else{
+                        writeRow("No","Friends");
+                    }
+                } catch (SQLException sql) {
+                    System.out.println("SQLException: " + sql.getMessage());
+                    System.out.println("SQLState: " + sql.getSQLState());
+                    System.out.println("Error: " + sql.getErrorCode());
+                    System.out.println("StackTrace: " + sql.getStackTrace());
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "We're sorry we detected an ERROR while connecting", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
                 }
-                rl.addView(createRow(friend, distanceStr));
             }
-        }else{
-            rl.addView(createRow("No","Friends"));
-        }
-        /*rl.addView(createRow("Giacomo", "5km"));
-        rl.addView(createRow("Pablo", "100m"));
-        rl.addView(createRow("Ramon", "2km"));*/
+        });
+        thread.start();
+
         this.addFriendsButton = (Button) findViewById(R.id.addFriends);
         this.addFriendsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,16 +107,38 @@ public class MainScreen extends Activity{
 
                     @Override
                     public void onClick(DialogInterface dialog, int whichButton) {
-                        //Introduce in the friend list (prevent the other user)
-                        if(Connect.dB.userExists(addUsername.getText().toString())) {
-                            Connect.dB.addFriend(Connect.username, addUsername.getText().toString());
-                        }else{
-                            runOnUiThread(new Runnable() {
-                                public void run() {
-                                    Toast.makeText(getApplicationContext(), "Error: Friend name doesn't exist", Toast.LENGTH_SHORT).show();
+                        Thread thread = new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    DataBaseInteraction dBInteraction = new DataBaseInteraction(AccountData.URLDB, AccountData.PASS, AccountData.USERNAME);
+                                    dBInteraction.connectToDB();
+
+                                    //Introduce in the friend list (prevent the other user)
+                                    if(dBInteraction.userExists(addUsername.getText().toString())) {
+                                        dBInteraction.addFriend(Connect.username, addUsername.getText().toString());
+                                    }else{
+                                        runOnUiThread(new Runnable() {
+                                            public void run() {
+                                                Toast.makeText(getApplicationContext(), "Error: Friend name doesn't exist", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                    }
+                                } catch (SQLException sql) {
+                                    System.out.println("SQLException: " + sql.getMessage());
+                                    System.out.println("SQLState: " + sql.getSQLState());
+                                    System.out.println("Error: " + sql.getErrorCode());
+                                    System.out.println("StackTrace: " + sql.getStackTrace());
+                                    runOnUiThread(new Runnable() {
+                                        public void run() {
+                                            Toast.makeText(getApplicationContext(), "We're sorry we detected an ERROR while connecting", Toast.LENGTH_LONG).show();
+                                        }
+                                    });
+
                                 }
-                            });
-                        }
+                            }
+                        });
+                        thread.start();
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -110,10 +156,32 @@ public class MainScreen extends Activity{
         this.buttonLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Change DB isConnected
-                if(Connect.dB.offline(Connect.username)) {
-                    finish();
-                }
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            DataBaseInteraction dBInteraction = new DataBaseInteraction(AccountData.URLDB, AccountData.PASS, AccountData.USERNAME);
+                            dBInteraction.connectToDB();
+
+                            //Change DB isConnected
+                            if(dBInteraction.offline(Connect.username)) {
+                                finish();
+                            }
+                        } catch (SQLException sql) {
+                            System.out.println("SQLException: " + sql.getMessage());
+                            System.out.println("SQLState: " + sql.getSQLState());
+                            System.out.println("Error: " + sql.getErrorCode());
+                            System.out.println("StackTrace: " + sql.getStackTrace());
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(), "We're sorry we detected an ERROR while connecting", Toast.LENGTH_LONG).show();
+                                }
+                            });
+
+                        }
+                    }
+                });
+                thread.start();
             }
         });
     }
@@ -147,6 +215,14 @@ public class MainScreen extends Activity{
         for(Person p: people){
            createRow(p.name, p.distance);
         }
+    }
+
+    public void writeRow(final String firstCol,final String secCol) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                createRow(firstCol,secCol);
+            }
+        });
     }
 
 }
